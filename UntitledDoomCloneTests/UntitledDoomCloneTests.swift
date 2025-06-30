e6a5fa6a016531fe2876de7bf39a110c45ab66e0
user-benjamin <benjamin.glover525@gmail.com>
2025-06-30 11:04:40 -0400

    Initial Commit

---
+++ UntitledDoomCloneTests/UntitledDoomCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  UntitledDoomCloneTests.swift
+//  UntitledDoomCloneTests
+//
+//  Created by Benjamin Glover on 6/30/25.
+//
+
+import Testing
+@testable import UntitledDoomClone
+
+struct UntitledDoomCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
